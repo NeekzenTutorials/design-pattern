6904865d9a23f6ba0b7dbb35457bf892fdf68a31
Neekzen <neekzen.62@gmail.com>
2024-05-15 16:12:29 +0200

    feat: enhance home page visual

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,5 @@
 <template>
-
-
-  <div class="home">
-
+  <div ref="home" class="home">
     <div class="content">
       <h1>{{ $t('welcomeMessage') }}</h1>
       <p>{{ $t('discover') }}</p>
@@ -13,47 +10,59 @@
     <div class="bubbles">
       <div class="bubble" v-for="n in 50" :key="n"></div>
     </div>
-
   </div>
 
+  <div class="bottom-arrow" @click="scrollToSection">
+    <div class="arrow" :class="{'up': scrolled}"></div>
+  </div>
 
-  <div class="patterns">
+  <div class="patterns" ref="patterns">
+    <h2 class="introduction">{{ $t('patternsIntroduction') }}</h2>
 
-  <h2 class="introduction">{{ $t('patternsIntroduction') }}</h2>
-
-  <section class="cards-wrapper">
-    <div v-for="pattern in patterns" :key="pattern.name" class="card">
-      <div class="card-content">
-        <h2>{{ $t(`pattern.${pattern.name}.name`) }}</h2>
-        <p>{{ $t(`pattern.${pattern.name}.description`) }}</p>
+    <section class="cards-wrapper">
+      <div v-for="pattern in patterns" :key="pattern.name" class="card">
+        <div class="card-content">
+          <h2>{{ $t(`pattern.${pattern.name}.name`) }}</h2>
+          <p>{{ $t(`pattern.${pattern.name}.description`) }}</p>
+        </div>
+        <router-link :to="`/pattern/${pattern.name}`" class="learn-more">{{ $t('learnMore') }}</router-link>
       </div>
-      <router-link :to="`/pattern/${pattern.name}`" class="learn-more">{{ $t('learnMore') }}</router-link>
+    </section>
+
+    <div class="area">
+      <ul class="circles">
+        <li v-for="i in 10" :key="i"></li>
+      </ul>
     </div>
-  </section>
-
-  <div class="area">
-    <ul class="circles">
-      <li v-for="i in 10" :key="i"></li>
-    </ul>
   </div>
-</div>
-
-
 </template>
 
 <script>
-import '../css/home/home.scss';
-
 export default {
   data() {
     return {
       patterns: [
         { name: 'Singleton' },
         { name: 'Factory' },
-      ]
+      ],
+      scrolled: false
+    };
+  },
+  methods: {
+    scrollToSection() {
+      const target = this.scrolled ? this.$refs.home : this.$refs.patterns;
+
+      // Ensure target is a valid DOM element
+      if (target && typeof target.scrollIntoView === 'function') {
+        target.scrollIntoView({ behavior: 'smooth' });
+        this.scrolled = !this.scrolled;
+      } else {
+        console.error('Invalid target for scrollIntoView:', target);
+      }
     }
   }
-}
+};
 </script>
+
 
 <style lang="scss" scoped src="../css/home/home.scss"></style>
